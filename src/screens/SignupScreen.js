4be41ea2c4228c60b9e70e2c578b23c0b2e33f41
yousefturin <yusefturin@gmail.com>
2024-04-08 03:52:03 +0300

    sign in system is create and connected to data base

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { View, Dimensions, Text, StyleSheet, SafeAreaView } from 'react-native'
+import { useAuth } from "../context/AuthContext";
+import { useNavigation } from "@react-navigation/native";
+import SvgComponent from "../utils/SvgComponents";
+import initializeScalingUtils from "../utils/NormalizeSize"
+import SinginForm from '../components/Singin/SinginForm';
+
+
+const { moderateScale } = initializeScalingUtils(Dimensions);
+
+export default function SignupScreen({ }) {
+    const navigation = useNavigation();
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#050505" }}>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <SvgComponent svgKey="LogoSVG" width={moderateScale(80)} height={moderateScale(80)} fill={'#ffffff'} />
+                </View>
+                <SinginForm navigation={navigation} />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+    },
+    header: {
+        justifyContent: "center",
+        alignItems: "center",
+        marginVertical: 50,
+        marginBottom: 30
+    },
+});
