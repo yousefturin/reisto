978bb6d2d90e6d4e7b5619799a1087e98cd251ab
yousefturin <yusefturin@gmail.com>
2024-04-15 10:58:54 +0300

    fonts load are removed.

--- App.js
+++ App.js
@@ -1,20 +1,7 @@
-import React, { useEffect } from "react";
-import * as Font from "expo-font";
+import React from "react";
 import AuthNavigation from "./src/navigation/AuthNavigation";
 
 export default function App() {
-  useEffect(() => {
-    async function loadFonts() {
-      await Font.loadAsync({
-        Montserrat: require("./assets/fonts/Montserrat-Regular.ttf"),
-        MontserratBold: require("./assets/fonts/Montserrat-Bold.ttf"),
-        TimesRoman: require("./assets/fonts/TimesRoman-Regular.ttf"),
-        lexend: require("./assets/fonts/Lexend-Regular.ttf")
-      });
-    }
-    loadFonts();
-  }, []);
   return <AuthNavigation />
-
 }
 
