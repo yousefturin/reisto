ca360e20114d7dc591c95aee317fe86372ef7a7a
yousefturin <yusefturin@gmail.com>
2024-04-15 10:44:25 +0300

    some changes regarding old code of nav-bar

--- src/screens/NotificationScreen.js
+++ src/screens/NotificationScreen.js
@@ -1,13 +1,9 @@
 import { SafeAreaView } from 'react-native'
 import React, { useState } from 'react'
-import NavigationStack from '../components/Home/Navigation'
-import { userData } from '../data/accountUser/myAccount'
 
 const NotificationScreen = () => {
-    const [activeButton, setActiveButton] = useState("Notification");
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: "#050505",justifyContent:"flex-end" }}>
-            <NavigationStack userData={userData}  activeButton={activeButton}/>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#050505", justifyContent: "flex-end" }}>
         </SafeAreaView>
     )
 }
