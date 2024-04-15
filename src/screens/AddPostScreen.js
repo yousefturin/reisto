da577072aed10e6a63cee48760bf3e98d472c140
yousefturin <yusefturin@gmail.com>
2024-04-15 10:42:32 +0300

    internal moves are enhanced.

--- src/screens/AddPostScreen.js
+++ src/screens/AddPostScreen.js
@@ -1,12 +1,10 @@
 import { SafeAreaView } from 'react-native'
 import React from 'react'
 import FormikPostUploader from '../components/NewPost/FormikPostUploader'
-import AddNewPostHeader from '../components/NewPost/AddNewPost'
 
 const AddPostScreen = () => {
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: "#050505"}}>
-            <AddNewPostHeader />
                 <FormikPostUploader />
         </SafeAreaView>
     )
