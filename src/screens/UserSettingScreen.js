03f885f4a6d0bddcbc926ff41fad3e197aa37ba3
yousefturin <yusefturin@gmail.com>
2024-04-15 10:48:46 +0300

    a new screen is added that is connected to user profile.

---
+++ src/screens/UserSettingScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const UserSettingScreen = () => {
+    return (
+        <View>
+            <Text>UserSettingScreen</Text>
+        </View>
+    )
+}
+
+export default UserSettingScreen
