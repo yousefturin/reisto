a078ac6846f632bc30be69180feaa33ba07fecd6
yousefturin <yusefturin@gmail.com>
2024-04-15 10:27:08 +0300

    navigation code syntax is improved for better reading.

--- src/components/Home/Navigation.js
+++ src/components/Home/Navigation.js
@@ -1,13 +1,24 @@
-import { View, Image, TouchableOpacity, Dimensions } from 'react-native'
-import React, { useState } from 'react'
+import { View, TouchableOpacity, Dimensions } from 'react-native'
+import { Image } from 'expo-image';
+import React from 'react'
 import SvgComponent from '../../utils/SvgComponents'
 import initializeScalingUtils from '../../utils/NormalizeSize';
 import { Divider } from 'react-native-elements';
 import { useNavigation } from "@react-navigation/native";
-
-const NavigationStack = ({ userData,activeButton }) => {
+import { blurHash } from '../../../assets/HashBlurData';
+const NavigationStack = ({ routeName, userData }) => {
     const { moderateScale } = initializeScalingUtils(Dimensions);
 
+    // profile image flickering issue is related to useEffect since each time the navigation is applied
+    // the useEffect will run once and that will make a problem the image must be taken and then it will be stored inside the async storage.
+    // The data will be stored in async storage and will be changed every time a user is changing the profile Image, or the user is
+    // signing out or logging in the the application to always fetch the newer Image.
+    // const [userData, setUserData] = useState([])
+
+    // Check if userData is available before rendering
+    if (!userData || !userData.profile_picture) {
+        return null; // Or any loading indicator
+    }
     const navigation = useNavigation();
     const icons = [
         {
@@ -36,63 +47,54 @@ const NavigationStack = ({ userData,activeButton }) => {
         },
     ]
     //#region Post Footer
-    const NavigationButtons = () => {
+
+    const NavigationButtons = ({ routeName }) => {
         return (
             <>
                 <Divider width={1} orientation='horizontal' color="#2b2b2b" />
-                <View style={{ flexDirection: "row", justifyContent: "space-around", height: 55, paddingTop: 5 }}>
-                    <TouchableOpacity onPress={() => {
-                        navigation.navigate("Home");
-
-                    }} style={{ padding: 10 }}>
-                        {/* HomeBTn */}
-                        <SvgComponent svgKey={activeButton === "Home" ? icons[0].activeURL : icons[0].inActiveURL} width={moderateScale(25)} height={moderateScale(25)} />
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => {
-                        navigation.navigate("Search");
-
-                    }} style={{ padding: 10 }}>
-                        {/* SearchBtn */}
-                        <SvgComponent svgKey={activeButton === "Search" ? icons[1].activeURL : icons[1].inActiveURL} width={moderateScale(25)} height={moderateScale(25)} />
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => {
-                        navigation.navigate("AddPost");
-
-                    }} style={{ padding: 10 }}>
-                        {/* AddPostBtn */}
-                        <SvgComponent svgKey={activeButton === "AddPost" ? icons[2].activeURL : icons[2].inActiveURL} width={moderateScale(25)} height={moderateScale(25)} />
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => {
-                        navigation.navigate("Notification");
-
-                    }} style={{ padding: 10 }}>
-                        {/* NotificationsBtn */}
-                        <SvgComponent svgKey={activeButton === "Notification" ? icons[3].activeURL : icons[3].inActiveURL} width={moderateScale(25)} height={moderateScale(25)} />
-                    </TouchableOpacity>
+                <View style={{ flexDirection: "row", justifyContent: "space-around", height: 80, paddingTop: 5, }}>
+                    {icons.slice(0, 4).map((icon, index) => (
+                        <TouchableOpacity
+                            key={index}
+                            onPress={() => {
+                                navigation.navigate(icon.action);
+                            }}
+                            style={{ padding: 10 }}
+                        >
+                            <SvgComponent
+                                svgKey={icon.action === routeName ? icon.activeURL : icon.inActiveURL}
+                                width={moderateScale(25)}
+                                height={moderateScale(25)}
+                            />
+                        </TouchableOpacity>
+                    ))}
                     <TouchableOpacity onPress={() => {
                         navigation.navigate("Profile");
 
                     }} style={{ padding: 10 }}>
                         {/* ProfileBtn */}
-                        <Image source={{ uri: icons[4].activeURL }} style={{
-                            width: moderateScale(25),
-                            height: moderateScale(25),
-                            borderRadius: 50,
-                            borderWidth: 1,
-                            borderColor: "#2b2b2b"
-                        }} />
-                    </TouchableOpacity>
+                        <Image source={{ uri: icons[4].activeURL, cache: "force-cache" }}
+                            style={{
+                                width: moderateScale(25),
+                                height: moderateScale(25),
+                                borderRadius: 50,
+                                borderWidth: 1,
+                                borderColor: "#2b2b2b"
+                                
+                            }} placeholder={blurHash}
+                            cachePolicy={"memory-disk"}
 
+                        />
+                    </TouchableOpacity>
                 </View>
             </>
-
         )
     }
     //#endregion
 
     return (
-        <View>
-            <NavigationButtons userData={userData} />
+        <View style={{ backgroundColor: "#050505" }}>
+            <NavigationButtons routeName={routeName} />
         </View>
     )
 }
