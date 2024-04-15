de98f0cfb39702bd7dc0b82ddc9d38ac7857542f
yousefturin <yusefturin@gmail.com>
2024-04-15 10:45:46 +0300

    profile screen is added and implemented.

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,11 +1,17 @@
-import { Button, SafeAreaView } from 'react-native'
-import React, { useState } from 'react'
-import NavigationStack from '../components/Home/Navigation'
-
-import { firebase } from '../firebase'
+import { RefreshControl, SafeAreaView, ScrollView } from 'react-native'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
+import { db, firebase } from '../firebase'
+import ProfileHeader from '../components/Profile/ProfileHeader'
+import { UserContext } from '../context/UserDataProvider'
+import ProfileContent from '../components/Profile/ProfileContent'
+import ProfilePost from '../components/Profile/ProfilePost'
 
 const ProfileScreen = () => {
-    const [activeButton, setActiveButton] = useState("Profile");
+    const userData = useContext(UserContext);
+    const [userPosts, setUserPost] = useState([])
+    const [refreshing, setRefreshing] = useState(false);
+    const [scrollToPostId, setScrollToPostId] = useState(null)
+    const [savePosts, setSavedPosts] = useState([])
     const handleLogout = async () => {
         try {
             await firebase.auth().signOut()
@@ -14,10 +20,58 @@ const ProfileScreen = () => {
             console.log(error)
         }
     }
+    useEffect(() => {
+        fetchUserPosts();
+    }, [])
+
+    const fetchUserPosts = () => {
+        const user = firebase.auth().currentUser;
+        if (user) {
+            db.collection('users').doc(user.email).collection('posts').orderBy('createdAt', 'desc').onSnapshot(snapshot => {
+                setUserPost(snapshot.docs.map(post => ({
+                    id: post.id,
+                    ...post.data()
+                })))
+            }, error => {
+                console.error("Error fetching posts:", error);
+            });
+        }
+        else {
+            console.error("No authenticated user found.");
+        }
+    };
+    // Function to handle scroll event
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        try {
+            fetchUserPosts(); // Your function to fetch posts
+        } catch (error) {
+            console.error('Error refreshing posts:', error);
+        }
+        setRefreshing(false);
+    }, []);
+    const handlePostPress = (postId) => {
+        setScrollToPostId(postId)
+    }
+
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: "#050505", justifyContent: "flex-end" }}>
-            <Button title='Logout' onPress={handleLogout} />
-            <NavigationStack userData={userData} activeButton={activeButton} />
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#050505" }}>
+            <>
+                <ProfileHeader handleLogout={handleLogout} userData={userData} />
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                    refreshControl={
+                        <RefreshControl
+                            refreshing={refreshing}
+                            onRefresh={onRefresh}
+                        />
+                    }
+                >
+                    <ProfileContent userData={userData} userPosts={userPosts} />
+                    <ProfilePost posts={userPosts} userData={userData} onPostPress={handlePostPress} savePosts={savePosts} />
+                </ScrollView>
+            </>
+
         </SafeAreaView>
     )
 }
