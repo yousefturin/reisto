685a821678194e6ed24403042708c44e7c927958
yousefturin <yusefturin@gmail.com>
2024-04-17 13:27:03 +0300

    svg component for close was fix to adapt the fill color.

--- assets/images/SVG/SvgStorage.js
+++ assets/images/SVG/SvgStorage.js
@@ -106,7 +106,7 @@ const svgData = {
   `,
   CloseSVG: `
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M5 5L19 19M5 19L19 5"  stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"
+    <path d="M5 5L19 19M5 19L19 5" stroke-width="1.5" stroke-linecap="round"
         stroke-linejoin="round"></path>
 </svg>`,
   DummyImage: `
