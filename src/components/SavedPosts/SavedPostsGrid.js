79de9c1873f6e62e6a90738706f771c53b932b35
yousefturin <yusefturin@gmail.com>
2024-04-17 16:50:31 +0300

    navigation for internal action are improved.

--- src/components/SavedPosts/SavedPostsGrid.js
+++ src/components/SavedPosts/SavedPostsGrid.js
@@ -1,6 +1,6 @@
 
-import { FlatList, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
-import React from 'react'
+import { FlatList, StyleSheet, Dimensions, TouchableOpacity, Animated } from 'react-native';
+import React, { useRef, useState } from 'react'
 import { Image } from 'expo-image';
 
 import { useNavigation } from "@react-navigation/native";
@@ -10,22 +10,41 @@ const columnCount = 3;
 const gapSize = 1;
 
 
-const SavedPostsGrid = ({ posts, userData }) => {
+const SavedPostsGrid = ({ posts, userData, navigateToScreen }) => {
     const navigation = useNavigation();
     const handleNavigationToPost = (postId) => {
-        navigation.navigate('UserSavedPostTimeLine', {
-            userData, scrollToPostId: postId
-        });
+        if (navigateToScreen === "SavedPosts") {
+            navigation.navigate('UserSavedPostTimeLine', {
+                userData, scrollToPostId: postId
+            });
+        } else if (navigateToScreen === "SearchExplore") {
+            navigation.navigate('ShareExplorePostTimeLine', {
+                userData, scrollToPostId: postId
+            });
+        }
     }
+    // const [loaded, setLoaded] = useState(false);
+    // const opacity = useRef(new Animated.Value(0)).current;
+    // // since there is no server to handle each image hash, then an animation is create 
+    // // to remove the flash when loading images and using this it will load all images together.
+    // const onLoad = () => {
+    //     Animated.timing(opacity, {
+    //         toValue: 1,
+    //         duration: 10, // Adjust the duration as needed
+    //         useNativeDriver: true,
+    //     }).start();
+    //     setLoaded(true);
+    // };
     const renderItem = ({ item }) => (
         <TouchableOpacity activeOpacity={0.8} onPress={() => handleNavigationToPost(item.id)}>
             <Image
                 source={{ uri: item.imageURL, cache: "force-cache" }}
-                style={styles.image}
-                placeholder={blurHash}
+                style={[styles.image]}
+                // placeholder={blurHash}
                 contentFit="cover"
                 cachePolicy={"memory-disk"}
                 recyclingKey={item.imageURL}
+                // onLoad={onLoad}
             />
         </TouchableOpacity>
     );
