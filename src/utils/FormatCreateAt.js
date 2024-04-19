4afc0a61f710f36049e236c6d21c1ed8d4034936
yousefturin <yusefturin@gmail.com>
2024-04-19 22:44:19 +0300

    utils that used in the about this user screen that returns the date in specific format.

---
+++ src/utils/FormatCreateAt.js
@@ -0,0 +1,6 @@
+export const formatCreatedAt = (date) => {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const year = date.getFullYear();
+    const month = months[date.getMonth()];
+    return `${month} ${year}`;
+};
