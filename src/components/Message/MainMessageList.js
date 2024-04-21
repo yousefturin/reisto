eca444eebec88863a40f4ed1945cacee85b91fba
yousefturin <yusefturin@gmail.com>
2024-04-21 22:56:15 +0300

    Refactor MainMessageList component to improve performance and readability

--- src/components/Message/MainMessageList.js
+++ src/components/Message/MainMessageList.js
@@ -1,67 +1,50 @@
 import { FlatList } from 'react-native'
-import React, { useCallback, useEffect, useState } from 'react'
+import React from 'react'
 import MessageMainItem from './MessageMainItem'
+import MessageLoadingPlaceHolder from './MessageLoadingPlaceHolder'
 
-const MainMessageList = ({ usersForMessaging, userData }) => {
-    const [sortedData, setSortedData] = useState([]);
-
-    // Update last message for a specific user
-    const updateLastMessage = useCallback((userId, message) => {
-        setSortedData(prevData => {
-            const newData = prevData.map(item => {
-                if (item.owner_uid === userId) {
-                    return {
-                        ...item,
-                        lastMessage: message
-                    };
+const MainMessageList = ({ updateLastMessage, userData, sortedData, usersForMessaging, flag }) => {
+    // this is the main list of messages that the user has
+    if (flag === "FromMain") {
+        return (
+            <FlatList
+                data={sortedData}
+                keyboardDismissMode="on-drag"
+                keyboardShouldPersistTaps={'always'}
+                renderItem={({ item, index }) => <MessageMainItem
+                    item={item} index={index}
+                    userData={userData}
+                    onUpdateLastMessage={updateLastMessage}
+                    flag={"FromMain"}
+                />}
+                keyExtractor={item => item.owner_uid.toString()}
+            />
+        )
+        // this is the list of users that the user can start a chat with
+    } if (flag === "FromNewMessage") {
+        return (
+            <>
+                {
+                    usersForMessaging.length !== 0 ? (
+                        <FlatList
+                            data={usersForMessaging}
+                            keyboardDismissMode="on-drag"
+                            keyboardShouldPersistTaps={'always'}
+                            renderItem={({ item, index }) => <MessageMainItem
+                                item={item} index={index}
+                                userData={userData}
+                                flag={"FromNewMessage"}
+                            />}
+                            keyExtractor={item => item.owner_uid.toString()}
+                        />
+                    ) : (
+                        <MessageLoadingPlaceHolder />
+                    )
                 }
-                return item;
-            });
+            </>
 
-            // Sort data based on lastMessage.createdAt
-            newData.sort((a, b) => {
-                const createdAtA = (a.lastMessage && a.lastMessage.createdAt) || null;
-                const createdAtB = (b.lastMessage && b.lastMessage.createdAt) || null;
-            
-                // Handle null values
-                if (createdAtA === null && createdAtB === null) {
-                    return 0;
-                } else if (createdAtA === null) {
-                    return 1; // Place items without createdAt at the end
-                } else if (createdAtB === null) {
-                    return -1; // Place items without createdAt at the end
-                }
-            
-                // Convert Firestore Timestamps to milliseconds
-                // hour of debugging 
-                const createdAtMillisA = createdAtA.toMillis();
-                const createdAtMillisB = createdAtB.toMillis();
-            
-                // Sort based on createdAtMillis
-                return createdAtMillisB - createdAtMillisA;
-            });
-
-            return newData;
-        });
-    }, []);
-
-    useEffect(() => {
-        // Initialize sorted data
-        setSortedData([...usersForMessaging]);
-    }, [usersForMessaging]);
-    return (
-        <FlatList
-            data={sortedData}
-            keyboardDismissMode="on-drag"
-            keyboardShouldPersistTaps={'always'}
-            renderItem={({ item, index }) => <MessageMainItem
-                item={item} index={index}
-                userData={userData}
-                onUpdateLastMessage={updateLastMessage}
-            />}
-            keyExtractor={item => item.owner_uid.toString()}
-        />
-    )
+        )
+    }
 }
 
 export default MainMessageList
