8e3f5b8e814a9f6e363eb69f4388496f81abadd5
yousefturin <yusefturin@gmail.com>
2024-04-21 22:56:27 +0300

    Update MessageMainHeader.js to include handleNavigationToNewMessage function

--- src/components/Message/MessageMainHeader.js
+++ src/components/Message/MessageMainHeader.js
@@ -4,20 +4,24 @@ import SvgComponent from '../../utils/SvgComponents'
 import initializeScalingUtils from '../../utils/NormalizeSize';
 import { useNavigation } from "@react-navigation/native";
 
-const MessageMainHeader = ({ userData }) => {
+const MessageMainHeader = ({ userData, excludedUsers }) => {
     const { moderateScale } = initializeScalingUtils(Dimensions);
     const navigation = useNavigation();
+    // This function will navigate to the new message screen
+    const handleNavigationToNewMessage = () => {
+        navigation.navigate("MessagingNewMessageForFollowerAndFollowings", { userData: userData, excludedUsers: excludedUsers });
+    }
     return (
         <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginHorizontal: 10 }}>
-            <View style={{flexDirection:"row",alignItems:"center"}}>
-                <TouchableOpacity onPress={()=>navigation.goBack()}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
                     <SvgComponent svgKey="ArrowBackSVG" width={moderateScale(40)} height={moderateScale(40)} />
                 </TouchableOpacity>
                 <Text style={{ color: "#fff", fontWeight: "800", fontSize: 25, }}>{userData.username}</Text>
             </View>
             <View style={{ margin: 10, height: moderateScale(30) }}>
             </View>
-            <TouchableOpacity style={{ marginHorizontal: 10,}}>
+            <TouchableOpacity style={{ marginHorizontal: 10 }} onPress={() => { handleNavigationToNewMessage() }}>
                 <SvgComponent svgKey="EditSVG" width={moderateScale(24)} height={moderateScale(24)} />
             </TouchableOpacity>
         </View>
