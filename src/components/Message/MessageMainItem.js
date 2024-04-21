0535286a7198ff12b4f0a59eee463835125b6b75
yousefturin <yusefturin@gmail.com>
2024-04-21 22:56:35 +0300

    Fix various issues and improve code readability

--- src/components/Message/MessageMainItem.js
+++ src/components/Message/MessageMainItem.js
@@ -1,5 +1,5 @@
 import { View, Text, TouchableOpacity, Alert, Dimensions } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Image } from 'expo-image';
 import { blurHash } from '../../../assets/HashBlurData';
 import { useNavigation } from '@react-navigation/native'
@@ -10,13 +10,13 @@ import { Skeleton } from 'moti/skeleton';
 import calculateTimeDifference from '../../utils/TimeDifferenceCalculator';
 import SvgComponent from '../../utils/SvgComponents';
 import initializeScalingUtils from '../../utils/NormalizeSize';
+import { MessagesNumContext } from '../../context/MessagesNumProvider';
 
-const MessageMainItem = ({ item, userData, onUpdateLastMessage }) => {
+const MessageMainItem = ({ item, userData, onUpdateLastMessage, flag }) => {
     const navigation = useNavigation();
     const [lastMessage, setLastMessage] = useState(null);
     const [loading, setLoading] = useState(true);
     const { moderateScale } = initializeScalingUtils(Dimensions);
-
     const SkeletonCommonProps = {
         colorMode: 'dark',
         backgroundColor: '#2b2b2b',
@@ -27,7 +27,7 @@ const MessageMainItem = ({ item, userData, onUpdateLastMessage }) => {
     }
 
     useEffect(() => {
-        let roomId = GenerateRoomId(userData?.owner_uid, item?.owner_uid);
+        let roomId = GenerateRoomId(userData.owner_uid, item.owner_uid);
         const DocRef = doc(db, 'messages', roomId);
         const messagesRef = collection(DocRef, "private_messages");
         const qu = query(messagesRef, orderBy('createdAt', 'desc'));
@@ -38,7 +38,7 @@ const MessageMainItem = ({ item, userData, onUpdateLastMessage }) => {
                 let allMessages = snapshot.docs.map(doc => doc.data());
                 const latestMessage = allMessages[0] || null;
                 setLastMessage(latestMessage);
-                onUpdateLastMessage(item.owner_uid, latestMessage);
+                if (flag === "FromMain") onUpdateLastMessage(item.owner_uid, latestMessage);
                 setLoading(false);
             });
         } catch (error) {
@@ -65,9 +65,14 @@ const MessageMainItem = ({ item, userData, onUpdateLastMessage }) => {
         if (userData?.owner_uid == lastMessage.owner_id) return <Text style={{ color: "#8E8E93", fontSize: 13, fontWeight: "500" }}>{"You: " + lastMessage?.text}</Text>
         // if not me the just show it normally
         return <View style={{ flexDirection: "row", gap: 5, }}>
-            <Text style={{ color: "#fff", fontSize: 13, fontWeight: "500" }} >{lastMessage?.text}</Text>
-            <View style={{alignSelf:"center"}}>
-                <SvgComponent svgKey="DotSVG" width={moderateScale(7)} height={moderateScale(7)} fill={'#ffffff'} />
+            <Text style={{ color: !lastMessage.seenBy.includes(userData.owner_uid) ? "#fff" : "#8E8E93", fontSize: 13, fontWeight: "500" }} >{lastMessage?.text}</Text>
+            {/* // if the user did not fetch the last message then show the dot as it indicate that the message is not seen yet by the user */}
+            <View style={{ alignSelf: "center" }}>
+                {!lastMessage.seenBy.includes(userData.owner_uid) ? (
+                    <SvgComponent svgKey="DotSVG" width={moderateScale(7)} height={moderateScale(7)} fill={'#ffffff'} />
+                ) : (
+                    null
+                )}
             </View>
         </View>
     }
@@ -80,45 +85,77 @@ const MessageMainItem = ({ item, userData, onUpdateLastMessage }) => {
             return ''; // or any default message or placeholder
         }
     }
-    return (
-        <TouchableOpacity style={{ flexDirection: "row", marginHorizontal: 10 }} onPress={() => handleNavigationToChat(item)}>
-            <View style={{ width: "20%", justifyContent: "center", alignItems: "center" }}>
-                <Image
-                    source={{ uri: item.profile_picture, cache: "force-cache" }}
-                    style={{
-                        width: 60,
-                        height: 60,
-                        borderRadius: 50,
-                        margin: 7,
-                        borderWidth: 1.5,
-                        borderColor: "#2b2b2b"
-                    }}
-                    placeholder={blurHash}
-                    transition={50}
-                    contentFit="cover"
-                    cachePolicy={"memory-disk"}
-                />
-            </View>
-            <View style={{ flexDirection: "column", width: "60%", justifyContent: "center", alignItems: "flex-start" }}>
-                <Text style={{ color: "#fff", fontWeight: "700", fontSize: 16 }}>{item.username}</Text>
-                {loading ? (
-                    <Skeleton
-                        show
-                        height={10}
-                        width={120}
-                        {...SkeletonCommonProps}
+    if (flag === "FromMain") {
+        return (
+            <TouchableOpacity style={{ flexDirection: "row", marginHorizontal: 10 }} onPress={() => handleNavigationToChat(item)}>
+                <View style={{ width: "20%", justifyContent: "center", alignItems: "center" }}>
+                    <Image
+                        source={{ uri: item.profile_picture, cache: "force-cache" }}
+                        style={{
+                            width: 60,
+                            height: 60,
+                            borderRadius: 50,
+                            margin: 7,
+                            borderWidth: 1.5,
+                            borderColor: "#2b2b2b"
+                        }}
+                        placeholder={blurHash}
+                        transition={50}
+                        contentFit="cover"
+                        cachePolicy={"memory-disk"}
                     />
-                ) : (
-                    <>
-                        {lastMessage ? renderLastMessage() : <Text style={{ color: "#8E8E93", fontSize: 13, fontWeight: "500" }}>Say Hi 👋</Text>}
-                    </>
-                )}
-            </View>
-            <View style={{ width: "20%", justifyContent: "center", alignItems: "center" }}>
-                <Text style={{ color: "#8E8E93", fontSize: 13, fontWeight: "500" }}>{lastMessage ? renderTime() : ''}</Text>
-            </View>
-        </TouchableOpacity>
-    );
+                </View>
+                <View style={{ flexDirection: "column", width: "60%", justifyContent: "center", alignItems: "flex-start" }}>
+                    <Text style={{ color: "#fff", fontWeight: "700", fontSize: 16 }}>{item.username}</Text>
+                    {loading ? (
+                        <Skeleton
+                            show
+                            height={10}
+                            width={120}
+                            {...SkeletonCommonProps}
+                        />
+                    ) : (
+                        <>
+                            {lastMessage ? renderLastMessage() : <Text style={{ color: "#8E8E93", fontSize: 13, fontWeight: "500" }}>Say Hi 👋</Text>}
+                        </>
+                    )}
+                </View>
+                <View style={{ width: "20%", justifyContent: "center", alignItems: "center" }}>
+                    <Text style={{ color: "#8E8E93", fontSize: 13, fontWeight: "500" }}>{lastMessage ? renderTime() : ''}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    }
+    if (flag === "FromNewMessage") {
+        return (
+            <TouchableOpacity style={{ flexDirection: "row", marginHorizontal: 10 }} onPress={() => handleNavigationToChat(item)}>
+                <View style={{ width: "20%", justifyContent: "center", alignItems: "center" }}>
+                    <Image
+                        source={{ uri: item.profile_picture, cache: "force-cache" }}
+                        style={{
+                            width: 60,
+                            height: 60,
+                            borderRadius: 50,
+                            margin: 7,
+                            borderWidth: 1.5,
+                            borderColor: "#2b2b2b"
+                        }}
+                        placeholder={blurHash}
+                        transition={50}
+                        contentFit="cover"
+                        cachePolicy={"memory-disk"}
+                    />
+                </View>
+                <View style={{ flexDirection: "column", width: "60%", justifyContent: "center", alignItems: "flex-start" }}>
+                    <Text style={{ color: "#fff", fontWeight: "700", fontSize: 16 }}>{item.username}</Text>
+                    {/* just to style the username in the center if there is no display_name to be shown */}
+                    {item.displayed_name ? <Text style={{ color: "#8E8E93", fontSize: 13, fontWeight: "500" }}>{item.displayed_name}</Text> : null}
+                </View>
+            </TouchableOpacity>
+        )
+    }
+
+
 };
 
 export default MessageMainItem
