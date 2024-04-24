65d96cb0a26482a3d470da42f87c57a10808d2d2
yousefturin <yusefturin@gmail.com>
2024-04-24 14:44:23 +0300

    Refactor fetchUserSavedPosts function in UserSavedPostScreen.js to include unsubscribe functionality

--- src/screens/UserSavedPostScreen.js
+++ src/screens/UserSavedPostScreen.js
@@ -11,16 +11,23 @@ const UserSavedPostScreen = () => {
     const [refreshing, setRefreshing] = useState(false);
 
     useEffect(() => {
-        fetchUserSavedPosts();
-    }, [])
+        const unsubscribe = fetchUserSavedPosts();
+
+        // Return cleanup function to unsubscribe when component unmounts
+        return () => {
+            unsubscribe();
+        };
+    }, []);
 
     // fetching here is different from UserSavedPostTimeLineScreen because it does not need the profile image to be displayed.
     // the fetching might be change for better and faster process<<<<<<<<<<<-.
     const fetchUserSavedPosts = () => {
         const user = firebase.auth().currentUser;
         if (user) {
+            const queryPost = db.collectionGroup('posts')
+            const querySavedPost = db.collection('users').doc(user.email).collection('saved_post')
             // bring all posts from across users
-            db.collectionGroup('posts').onSnapshot(querySnapshot => {
+            return queryPost.onSnapshot(querySnapshot => {
                 // create an array and push all the post inside of it that will be used later for matching saved posts with posts that are fetched 
                 const allPosts = [];
                 querySnapshot.forEach(doc => {
@@ -30,7 +37,7 @@ const UserSavedPostScreen = () => {
                     });
                 });
                 // fetch user saved posts collection and bring the data 
-                db.collection('users').doc(user.email).collection('saved_post').get().then(snapshot => {
+                querySavedPost.get().then(snapshot => {
                     const savedPostDoc = snapshot.docs[0];
                     // if the data exist 
                     if (savedPostDoc) {
@@ -53,6 +60,8 @@ const UserSavedPostScreen = () => {
             })
         } else {
             console.error("No authenticated user found.");
+            return () => { };
+
         }
     };
 
@@ -73,8 +82,10 @@ const UserSavedPostScreen = () => {
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: "#050505" }}>
             <>
-                <SavedPostsHeader header={"Saved Posts"}/>
+                <SavedPostsHeader header={"Saved Posts"} />
                 <ScrollView
+                    keyboardDismissMode="on-drag"
+                    keyboardShouldPersistTaps={'always'}
                     showsVerticalScrollIndicator={false}
                     refreshControl={
                         <RefreshControl
