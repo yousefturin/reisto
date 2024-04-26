190c52838f2dc9585ff6d2815981dc43bb867dfd
yousefturin <yusefturin@gmail.com>
2024-04-26 18:43:59 +0300

    refactored the code,added the function for showing share users.

--- src/screens/UserSavedPostTimeLineScreen.js
+++ src/screens/UserSavedPostTimeLineScreen.js
@@ -1,5 +1,5 @@
 import { Dimensions, FlatList, SafeAreaView } from 'react-native'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import Post from '../components/Home/Post'
 import { db, firebase } from '../firebase';
 import SavedPostsHeader from '../components/SavedPosts/SavedPostsHeader';
@@ -11,6 +11,7 @@ const UserSavedPostTimeLineScreen = ({ route }) => {
     const [savedPosts, setSavedPosts] = useState([])
     const flatListRef = useRef();
     const [initialScrollIndex, setInitialScrollIndex] = useState(null);
+    const [usersForSharePosts, setUsersForSharePosts] = useState([]);
 
     const handleScrollToIndexFailed = info => {
         const wait = new Promise(resolve => setTimeout(resolve, 500));
@@ -35,85 +36,136 @@ const UserSavedPostTimeLineScreen = ({ route }) => {
     }, [savedPosts, scrollToPostId]);
 
     useEffect(() => {
-        const unsubscribe = fetchUserSavedPosts();
+        let unsubscribe;
 
+        const fetchUserSavedPosts = () => {
+            const user = firebase.auth().currentUser;
+            if (user) {
+                const queryPost = db.collectionGroup('posts')
+                const querySavedPost = db.collection('users').doc(user.email).collection('saved_post')
+                // bring all posts from across users
+                return queryPost.onSnapshot(querySnapshot => {
+                    // create an array and push all the post inside of it that will be used later for matching saved posts with posts that are fetched 
+                    const allPosts = [];
+                    querySnapshot.forEach(doc => {
+                        allPosts.push({
+                            id: doc.id,
+                            ...doc.data()
+                        });
+                    });
+                    // fetch user saved posts collection and bring the data 
+                    querySavedPost.get().then(snapshot => {
+                        const savedPostDoc = snapshot.docs[0];
+                        // if the data exist 
+                        if (savedPostDoc) {
+                            // get the ids array from the data 
+                            const postIds = savedPostDoc.data().saved_post_id;
+                            // if the array data and it is an array 
+                            if (postIds && Array.isArray(postIds)) {
+                                // filter from the posts array the once that has the same stored id 
+                                const savedPostsData = allPosts.filter(post => postIds.includes(post.id));
+
+                                // Fetch profile pictures for the saved posts
+                                const postsWithProfilePictures = savedPostsData.map(async post => {
+                                    const dbPostData = post;
+                                    try {
+                                        const userDoc = await db.collection('users').doc(post.owner_email).get()
+                                        const dbUserData = userDoc.data()
+                                        const dbProfilePicture = dbUserData.profile_picture
+                                        return {
+                                            ...dbPostData,
+                                            profile_picture: dbProfilePicture
+                                        }
+                                    } catch (error) {
+                                        console.error('Error fetching user document:', error)
+                                        // If fetching profile picture fails, continue with the post data without it
+                                        return dbPostData;
+                                    }
+                                });
+
+                                Promise.all(postsWithProfilePictures).then(posts => {
+                                    setSavedPosts(posts);
+                                }).catch(error => {
+                                    console.error('Error fetching saved posts with profile pictures:', error);
+                                });
+                            } else {
+                                console.error('Invalid or empty post IDs array');
+                            }
+                        } else {
+                            console.log('No saved post document found for the user');
+                        }
+                    }).catch(error => {
+                        console.error("Error fetching saved posts:", error);
+                    });
+                })
+            } else {
+                console.error("No authenticated user found.");
+                return () => { };
+            }
+        };
+        fetchUserSavedPosts();
         // Return cleanup function to unsubscribe when component unmounts
         return () => {
-            unsubscribe();
+            unsubscribe && unsubscribe();
         };
     }, []);
 
+    useLayoutEffect(() => {
+        fetchData();
+    }, []);
+    // if this function is not duplicated across the app, and instead placed in Post Component it will slow down the performance, since it will 
+    // be called for each post, and it will be called for each post that is rendered on the screen, but using it in each parent component is better.
+    const fetchData = async () => {
+        try {
 
-    const fetchUserSavedPosts = () => {
-        const user = firebase.auth().currentUser;
-        if (user) {
-            const queryPost = db.collectionGroup('posts')
-            const querySavedPost = db.collection('users').doc(user.email).collection('saved_post')
-            // bring all posts from across users
-            return queryPost.onSnapshot(querySnapshot => {
-                // create an array and push all the post inside of it that will be used later for matching saved posts with posts that are fetched 
-                const allPosts = [];
-                querySnapshot.forEach(doc => {
-                    allPosts.push({
-                        id: doc.id,
-                        ...doc.data()
-                    });
-                });
-                // fetch user saved posts collection and bring the data 
-                querySavedPost.get().then(snapshot => {
-                    const savedPostDoc = snapshot.docs[0];
-                    // if the data exist 
-                    if (savedPostDoc) {
-                        // get the ids array from the data 
-                        const postIds = savedPostDoc.data().saved_post_id;
-                        // if the array data and it is an array 
-                        if (postIds && Array.isArray(postIds)) {
-                            // filter from the posts array the once that has the same stored id 
-                            const savedPostsData = allPosts.filter(post => postIds.includes(post.id));
+            const querySnapshot = await db.collection('users').doc(firebase.auth().currentUser.email).collection('following_followers').limit(1).get();
+            if (!querySnapshot.empty) {
+                const doc = querySnapshot.docs[0];
+                const data = doc.data();
 
-                            // Fetch profile pictures for the saved posts
-                            const postsWithProfilePictures = savedPostsData.map(async post => {
-                                const dbPostData = post;
-                                try {
-                                    const userDoc = await db.collection('users').doc(post.owner_email).get()
-                                    const dbUserData = userDoc.data()
-                                    const dbProfilePicture = dbUserData.profile_picture
-                                    return {
-                                        ...dbPostData,
-                                        profile_picture: dbProfilePicture
-                                    }
-                                } catch (error) {
-                                    console.error('Error fetching user document:', error)
-                                    // If fetching profile picture fails, continue with the post data without it
-                                    return dbPostData;
-                                }
-                            });
+                const fetchPromises = [];
+                const fetchPromisesSecond = [];
 
-                            Promise.all(postsWithProfilePictures).then(posts => {
-                                setSavedPosts(posts);
-                            }).catch(error => {
-                                console.error('Error fetching saved posts with profile pictures:', error);
-                            });
-                        } else {
-                            console.error('Invalid or empty post IDs array');
-                        }
+                for (const follower of data.followers) {
+                    const fetchPromise = db.collection('users').doc(follower).get();
+                    fetchPromises.push(fetchPromise);
+                }
+
+                for (const following of data.following) {
+                    const fetchPromise = db.collection('users').doc(following).get();
+                    fetchPromisesSecond.push(fetchPromise);
+                }
+
+                const [followerDocs, followingDocs] = await Promise.all([Promise.all(fetchPromises), Promise.all(fetchPromisesSecond)]);
+
+                const followersData = followerDocs.filter(doc => doc.exists).map(doc => doc.data());
+                const followingData = followingDocs.filter(doc => doc.exists).map(doc => doc.data());
+
+                const allUsersData = [...followersData, ...followingData];
+                const uniqueUserIds = new Set();
+                const filteredUsersData = allUsersData.filter(user => {
+                    if (uniqueUserIds.has(user.owner_uid)) {
+                        return false;
                     } else {
-                        console.log('No saved post document found for the user');
+                        uniqueUserIds.add(user.owner_uid);
+                        return true;
                     }
-                }).catch(error => {
-                    console.error("Error fetching saved posts:", error);
                 });
-            })
-        } else {
-            console.error("No authenticated user found.");
-            return () => { };
+
+                setUsersForSharePosts(filteredUsersData);
+            } else {
+                console.log("No document found in the collection.");
+            }
+        } catch (error) {
+            console.error("Error fetching data:", error);
         }
     };
 
 
 
+
     const renderItem = ({ item }) => (
-        <Post post={item} userData={userData} />
+        <Post post={item} userData={userData} usersForSharePosts={usersForSharePosts} />
     )
 
 
