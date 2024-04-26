b025be8b0229bcd11c10e2898aa7b199b8f2f4a8
yousefturin <yusefturin@gmail.com>
2024-04-26 18:41:15 +0300

    Refactor FromMessagesToSharedPost.js to add new screen FromMessagesToSharedPost Refactor AboutThisUserScreen.js to fetch user data using ownerID in AboutThisUserScreen.js Refactor AppNavigation.js to add new screen FromMessagesToSharedPost Refactor OthersProfileContent.js to fetch user data using user ID instead of route in OthersProfileContent.js Refactor MessageMainItem component to improve last message rendering Refactor MessageMainItem component to improve last message rendering in MessageMainItem.js Refactor LoadingPlaceHolder component to conditionally render the footer in LoadingPlaceHolder.js Refactor SVG variable names in SvgStorage.js Added new icon image for the application Update app.json to remove unused splash image and update favicon path Refactor error logging in UserProfileScreen.js Refactor MessagingIndividualScreen.js to use const for unsubscribe variable declaration Remove commented out code and unused import statements in Modals.js Refactor EditProfileImage.js to remove commented out code and unused import statements Fix handleUserFollowingAndFollowerDisplay function in ProfileContent.js Remove console.log statements and unsubscribe function in OthersProfileContent.js Refactor LoadingPlaceHolder component to conditionally render the footer in LoadingPlaceHolder.js Refactor SVG variable names in SvgStorage.js added new icon image for the application

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,8 @@
 import { SafeAreaView, RefreshControl, FlatList, View, Text } from 'react-native'
-import React, { useEffect, useState, useCallback, useContext } from 'react'
+import React, { useEffect, useState, useCallback, useContext, useLayoutEffect } from 'react'
 import Header from '../components/Home/Header'
 import Post from '../components/Home/Post'
-import { db } from '../firebase'
+import { db, firebase } from '../firebase'
 import { UserContext } from '../context/UserDataProvider'
 import LoadingPlaceHolder from '../components/Home/LoadingPlaceHolder'
 
@@ -10,74 +10,138 @@ import LoadingPlaceHolder from '../components/Home/LoadingPlaceHolder'
 const HomeScreen = () => {
     const [refreshing, setRefreshing] = useState(false);
     const [posts, setPosts] = useState([])
+    const [usersForSharePosts, setUsersForSharePosts] = useState([]);
     const userData = useContext(UserContext);
 
     useEffect(() => {
+        // Call fetchPost when the component mounts
         const unsubscribe = fetchPost();
-    
-        // Return cleanup function to unsubscribe when component unmounts
+        console.log("Subscribed to posts.");
+        // Return a cleanup function to unsubscribe when the component unmounts
         return () => {
-            unsubscribe();
+            if (unsubscribe) {
+                unsubscribe();
+                console.log("Unsubscribed from posts.");
+            }
         };
     }, []);
-    
-    const fetchPost = () => {
-        // Initialize variable to store reference to the listener
-        let unsubscribe = null;
-    
-        // Assign the listener and store the reference
-        unsubscribe = db.collectionGroup('posts').orderBy('createdAt', 'desc').onSnapshot(snapshot => {
-            const postsWithProfilePictures = snapshot.docs.map(async post => {
-                const dbPostData = post.data();
-                try {
-                    const userDoc = await db.collection('users').doc(dbPostData.owner_email).get()
-                    const dbUserData = userDoc.data()
-                    const dbProfilePicture = dbUserData.profile_picture
-                    return {
-                        id: post.id,
-                        profile_picture: dbProfilePicture,
-                        ...dbPostData
-                    }
-                } catch (error) {
-                    console.error('Error fetching user document:', error)
-                    return {
-                        id: post.id,
-                        ...dbPostData // Fallback to original post data if user document fetch fails
-                    }
-                }
-            })
-            Promise.all(postsWithProfilePictures).then(posts => {
-                setPosts(posts)
-            }).catch(error => {
-                console.error('Error fetching posts with profile pictures:', error);
-            })
-        });
-    
-        // Return a function to unsubscribe when component unmounts or user logs out
-        return unsubscribe;
-    };
 
+    const fetchPost = useCallback(() => {
+        const user = firebase.auth().currentUser;
+        // Assign the listener and store the reference
+        if (user) {
+            return db.collectionGroup('posts').orderBy('createdAt', 'desc').onSnapshot(snapshot => {
+                const postsWithProfilePictures = snapshot.docs.map(async post => {
+                    const dbPostData = post.data();
+                    try {
+                        const userDoc = await db.collection('users').doc(dbPostData.owner_email).get()
+                        const dbUserData = userDoc.data()
+                        const dbProfilePicture = dbUserData.profile_picture
+                        return {
+                            id: post.id,
+                            profile_picture: dbProfilePicture,
+                            ...dbPostData
+                        }
+                    } catch (error) {
+                        console.error('Error fetching user document:', error)
+                        return {
+                            id: post.id,
+                            ...dbPostData // Fallback to original post data if user document fetch fails
+                        }
+                    }
+                })
+                Promise.all(postsWithProfilePictures).then(posts => {
+                    setPosts(posts)
+                }).catch(error => {
+                    console.error('Error fetching posts with profile pictures:', error);
+                })
+            });
+        } else {
+            console.error("No authenticated user found.");
+            return null; // Return null if user is not authenticated
+        }
+    }, []);
+    // this function need a fix for cleanup after mount
     // Function to handle scroll event
     const onRefresh = useCallback(() => {
         setRefreshing(true);
         try {
-            fetchPost(); // Your function to fetch posts
+            // Fetch posts again
+            const unsubscribe = fetchPost();
+            console.log("Refreshing posts...");
+            // Return a cleanup function to unsubscribe when the fetch is done
+            return () => {
+                if (unsubscribe) {
+                    unsubscribe();
+                    console.log("Unsubscribed from posts after refreshing.");
+                }
+            };
         } catch (error) {
             console.error('Error refreshing posts:', error);
+            return () => { };
+        } finally {
+            setRefreshing(false);
         }
-        setRefreshing(false);
+    }, [fetchPost]);
+
+    useLayoutEffect(() => {
+        fetchData();
     }, []);
 
+    const fetchData = async () => {
+        try {
+            const querySnapshot = await db.collection('users').doc(firebase.auth().currentUser.email).collection('following_followers').limit(1).get();
+            if (!querySnapshot.empty) {
+                const doc = querySnapshot.docs[0];
+                const data = doc.data();
+
+                const fetchPromises = [];
+                const fetchPromisesSecond = [];
+
+                for (const follower of data.followers) {
+                    const fetchPromise = db.collection('users').doc(follower).get();
+                    fetchPromises.push(fetchPromise);
+                }
+
+                for (const following of data.following) {
+                    const fetchPromise = db.collection('users').doc(following).get();
+                    fetchPromisesSecond.push(fetchPromise);
+                }
+
+                const [followerDocs, followingDocs] = await Promise.all([Promise.all(fetchPromises), Promise.all(fetchPromisesSecond)]);
+
+                const followersData = followerDocs.filter(doc => doc.exists).map(doc => doc.data());
+                const followingData = followingDocs.filter(doc => doc.exists).map(doc => doc.data());
+
+                const allUsersData = [...followersData, ...followingData];
+                const uniqueUserIds = new Set();
+                const filteredUsersData = allUsersData.filter(user => {
+                    if (uniqueUserIds.has(user.owner_uid)) {
+                        return false;
+                    } else {
+                        uniqueUserIds.add(user.owner_uid);
+                        return true;
+                    }
+                });
+
+                setUsersForSharePosts(filteredUsersData);
+            } else {
+                console.log("No document found in the collection.");
+            }
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: "#050505" }}>
             <Header />
             {posts.length !== 0 ? (
                 <FlatList
                     keyboardDismissMode="on-drag"
-                    keyboardShouldPersistTaps='handled' 
+                    keyboardShouldPersistTaps='handled'
                     data={posts}
                     renderItem={({ item, index }) => (
-                        <Post post={item} key={index} isLastPost={index === posts.length - 1} userData={userData} />
+                        <Post post={item} key={index} isLastPost={index === posts.length - 1} userData={userData} usersForSharePosts={usersForSharePosts} />
                     )}
                     keyExtractor={(item, index) => index.toString()}
                     refreshControl={
