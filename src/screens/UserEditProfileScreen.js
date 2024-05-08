bd79a60586dd1de8a90f02153ce9bf896ed4bfbe
yousefturin <yusefturin@gmail.com>
2024-05-08 22:29:54 +0300

    Refactor UserEditProfileScreen to use colorPalette for consistent color usage and improve code readability

--- src/screens/UserEditProfileScreen.js
+++ src/screens/UserEditProfileScreen.js
@@ -5,18 +5,19 @@ import { useNavigation } from "@react-navigation/native";
 import EditProfileHeader from '../components/UserEditProfile/EditProfileHeader';
 import EditProfileForm from '../components/UserEditProfile/EditProfileForm';
 import EditProfileImage from '../components/UserEditProfile/EditProfileImage';
+import { colorPalette } from '../Config/Theme';
 
 const UserEditProfileScreen = ({ route }) => {
     const { userData } = route.params;
     const navigation = useNavigation();
     return (
-        <SafeAreaView style={{ backgroundColor: "#050505", flex: 1, justifyContent: "flex-start" }}>
+        <SafeAreaView style={{ backgroundColor: colorPalette.dark.Primary , flex: 1, justifyContent: "flex-start" }}>
             <EditProfileHeader navigation={navigation} headerTitle={"Edit profile"} />
-            <Divider width={0.4} orientation='horizontal' color="#2b2b2b" />
+            <Divider width={0.4} orientation='horizontal' color={colorPalette.dark.dividerPrimary}  />
             <EditProfileImage userData={userData} />
-            <Divider width={0.4} orientation='horizontal' color="#2b2b2b" />
+            <Divider width={0.4} orientation='horizontal' color={colorPalette.dark.dividerPrimary}  />
             <EditProfileForm userData={userData} navigation={navigation} />
-            <Divider width={0.5} orientation='horizontal' color="#2b2b2b" />
+            <Divider width={0.5} orientation='horizontal' color={colorPalette.dark.dividerPrimary}  />
         </SafeAreaView>
     )
 }
