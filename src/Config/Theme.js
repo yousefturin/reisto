cef80e32174ae607f3e0a5ad38389ba4a6398ce9
yousefturin <yusefturin@gmail.com>
2024-05-08 22:25:56 +0300

    Refactor code to use colorPalette for consistent color usage and improve code readability

---
+++ src/Config/Theme.js
@@ -0,0 +1,54 @@
+// Desc: Theme configuration for the app
+
+export const colorPalette = {
+    light: {},
+    dark: {
+        Primary: '#050505',
+        SubPrimary: '#262626',
+        Secondary: '#2b2b2b',
+        SubSecondary: '#222',
+        Tertiary: '#383838',
+        Quaternary: '#F8F8FC',
+        Quinary: '#1C1C1E',
+
+        textPrimary: '#ffffff',
+        textSubPrimary: '#f2f2f7',
+        textSecondary: '#8E8E93',
+        textTertiary: '#656565',
+        textQuaternary: '#dddddd',
+        textQuinary: '#b2b2b9',
+        textError: '#ed4535',
+        textURL: '#d8e0fa',
+
+        btnPrimary: '#515151',
+
+        textPlaceholder: '#383838',
+        textPlaceholderSecondary: '#D3D3D6',
+
+        modalBackgroundPrimary: '#2b2b2b',
+        modalBtn: '#4c4c4c',
+        modalDivider: '#575757',
+
+        dividerPrimary: '#2b2b2b',
+        dividerSecondary: '#383838',
+
+        notch: "#727272",
+
+        appPrimary: '#007AFF',
+        appSubPrimary: '#1c87fc',
+        appSecondary: '#add1f7',
+
+        appGradientPrimary: '#7e9bdf',
+        appGradientSecondary: '#6581B7',
+        appGradientTertiary: '#445379',
+
+        appGradientPrimary2: '#526591',
+        appGradientSecondary2: '#304062',
+        appGradientTertiary2: '#2D3751',
+
+        appInactiveGradientPrimary: '#bebebe',
+        appInactiveGradientSecondary: '#727272',
+        appInactiveGradientTertiary: '#4c4c4c',
+
+    }
+}
