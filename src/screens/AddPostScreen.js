84924514fdc3ee8c8d66af19a44038ac5093eae9
yousefturin <yusefturin@gmail.com>
2024-05-08 22:26:17 +0300

    Refactor AddPostScreen.js to use colorPalette for consistent color usage and improve code readability

--- src/screens/AddPostScreen.js
+++ src/screens/AddPostScreen.js
@@ -1,10 +1,11 @@
 import { SafeAreaView } from 'react-native'
 import React from 'react'
 import FormikPostUploader from '../components/NewPost/FormikPostUploader'
+import { colorPalette } from '../Config/Theme'
 
 const AddPostScreen = () => {
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: "#050505"}}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: colorPalette.dark.Primary }}>
                 <FormikPostUploader />
         </SafeAreaView>
     )
