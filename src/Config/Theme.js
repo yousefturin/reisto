c2f9f1f4029cc7081f56341896cd229105f7a151
yousefturin <yusefturin@gmail.com>
2024-05-13 13:00:53 +0300

    Refactor colorPalette in Theme.js for consistent color usage and improved code readability

--- src/Config/Theme.js
+++ src/Config/Theme.js
@@ -1,8 +1,62 @@
 // Desc: Theme configuration for the app
 
 export const colorPalette = {
-    light: {},
+    light: {
+        // colors related to backgrounds
+        Primary: '#fefffe',
+        SubPrimary: '#EFEFEF',
+        Secondary: '#f2f2f7',
+        SubSecondary: '#fefffe',
+        Tertiary: '#dddddd',
+        Quaternary: '#F8F8FC',
+        Quinary: '#f2f2f7',
+
+        // colors related to texts
+        textPrimary: '#000',
+        textSubPrimary: '#000',
+        textSecondary: '#8E8E93',
+        textTertiary: '#656565',
+        textQuaternary: '#515151',
+        textQuinary: '#b2b2b9',
+        textError: '#ed4535',
+        textURL: '#5d90d0',
+
+        // colors related to buttonsD
+        btnPrimary: '#515151',
+
+        // colors related to placeholder
+        textPlaceholder: '#D3D3D6',
+        textPlaceholderSecondary: '#D3D3D6',
+
+        //colors related to modal
+        modalBackgroundPrimary: '#2b2b2b',
+        modalBtn: '#e3e3e3',
+        modalDivider: '#d6d6df',
+        notch: "#b2b2b9",
+
+        // colors related to divider
+        dividerPrimary: "rgba(198, 198, 200, 0.45)",
+        dividerSecondary: "rgba(198, 198, 200, 0.45)",
+
+        // colors related to app main colors
+        appPrimary: '#007AFF',
+        appSubPrimary: '#1c87fc',
+        appSecondary: '#add1f7',
+
+        appGradientPrimary: '#7e9bdf',
+        appGradientSecondary: '#6581B7',
+        appGradientTertiary: '#445379',
+
+        appGradientPrimary2: '#526591',
+        appGradientSecondary2: '#304062',
+        appGradientTertiary2: '#2D3751',
+
+        appInactiveGradientPrimary: '#bebebe',
+        appInactiveGradientSecondary: '#727272',
+        appInactiveGradientTertiary: '#4c4c4c',
+    },
     dark: {
+        // colors related to backgrounds
         Primary: '#050505',
         SubPrimary: '#262626',
         Secondary: '#2b2b2b',
@@ -11,6 +65,7 @@ export const colorPalette = {
         Quaternary: '#F8F8FC',
         Quinary: '#1C1C1E',
 
+        // colors related to texts
         textPrimary: '#ffffff',
         textSubPrimary: '#f2f2f7',
         textSecondary: '#8E8E93',
@@ -20,20 +75,24 @@ export const colorPalette = {
         textError: '#ed4535',
         textURL: '#d8e0fa',
 
+        // colors related to buttons
         btnPrimary: '#515151',
 
+        // colors related to placeholder
         textPlaceholder: '#383838',
         textPlaceholderSecondary: '#D3D3D6',
 
+        //colors related to modal
         modalBackgroundPrimary: '#2b2b2b',
         modalBtn: '#4c4c4c',
         modalDivider: '#575757',
+        notch: "#727272",
 
+        // colors related to divider
         dividerPrimary: '#2b2b2b',
         dividerSecondary: '#383838',
 
-        notch: "#727272",
-
+        // colors related to app main colors
         appPrimary: '#007AFF',
         appSubPrimary: '#1c87fc',
         appSecondary: '#add1f7',
