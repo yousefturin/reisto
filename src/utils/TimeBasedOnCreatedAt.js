aaae54a68b5035aacfa63263a2c8a7eb8e06e2a7
yousefturin <yusefturin@gmail.com>
2024-05-16 17:02:20 +0300

    refactor: Update TimeBasedOnCreatedAt.js to calculate creation time based on provided time object

--- src/utils/TimeBasedOnCreatedAt.js
+++ src/utils/TimeBasedOnCreatedAt.js
@@ -1,3 +1,8 @@
+/**
+ * Calculates the creation time based on the provided time object.
+ * @param {Object} timeObject - The time object containing the seconds and nanoseconds.
+ * @returns {string} - The formatted time string in the format 'hh:mm AM/PM'.
+ */
 export const CalculateCreateAt = (timeObject) => {
     const { seconds, nanoseconds } = timeObject;
     const date = new Date(seconds * 1000 + nanoseconds / 1000000);
