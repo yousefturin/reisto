11cf1381d58a70e6262bef58c092ecab31b14fe5
yousefturin <yusefturin@gmail.com>
2024-05-16 17:02:29 +0300

    refactor: Update TimeDifferenceCalculator.js to calculate time difference in a human-readable format

--- src/utils/TimeDifferenceCalculator.js
+++ src/utils/TimeDifferenceCalculator.js
@@ -1,4 +1,10 @@
-function calculateTimeDifference(timestampObj) {
+/**
+ * Calculates the time difference between a given timestamp and the current time.
+ * @param {Object} timestampObj - The Firestore timestamp object.
+ * @param {Function} t - The translation function for retrieving localized strings.
+ * @returns {string} - The calculated time difference in a human-readable format.
+ */
+function calculateTimeDifference(timestampObj, t) {
     // Convert Firestore timestamp to JavaScript Date object
     const targetTime = new Date((timestampObj.seconds * 1000) + (timestampObj.nanoseconds / 1000000));
 
@@ -7,19 +13,19 @@ function calculateTimeDifference(timestampObj) {
     const differenceInSeconds = Math.floor(differenceInMilliseconds / 1000);
 
     if (differenceInSeconds < 60) {
-        return `${differenceInSeconds}s`;
+        return `${differenceInSeconds}${t('screens.home.text.timeCalculation.second')}`;
     } else {
         const differenceInMinutes = Math.floor(differenceInSeconds / 60);
         if (differenceInMinutes < 60) {
-            return `${differenceInMinutes}m`;
+            return `${differenceInMinutes}${t('screens.home.text.timeCalculation.minute')}`;
         } else {
             const differenceInHours = Math.floor(differenceInMinutes / 60);
             if (differenceInHours < 24) {
-                return `${differenceInHours}h`;
+                return `${differenceInHours}${t('screens.home.text.timeCalculation.hour')}`;
             } else {
                 const differenceInDays = Math.floor(differenceInHours / 24);
                 if (differenceInDays < 30) {
-                    return `${differenceInDays}d`;
+                    return `${differenceInDays}${t('screens.home.text.timeCalculation.day')}`;
                 } else {
                     // If more than one month, return the date
                     const options = { year: 'numeric', month: 'short', day: 'numeric' };
