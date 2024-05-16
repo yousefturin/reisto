7fa6d442d8f4141f8d9c78f717738e38a50613a2
yousefturin <yusefturin@gmail.com>
2024-05-16 16:53:45 +0300

    refactor: Update MessageMainList component to handle loading state and display empty data message

--- src/components/Message/MessageMainList.js
+++ src/components/Message/MessageMainList.js
@@ -2,21 +2,22 @@ import { FlatList } from 'react-native'
 import React from 'react'
 import MessageMainItem from './MessageMainItem'
 import MessageLoadingPlaceHolder from './MessageLoadingPlaceHolder'
+import EmptyDataParma from '../CustomComponent/EmptyDataParma'
 
-const MessageMainList = ({ updateLastMessage, userData, sortedData, usersForMessaging, flag, theme }) => {
+const MessageMainList = ({ updateLastMessage, userData, sortedData, usersForMessaging, flag, theme, t, loading }) => {
     // this is the main list of messages that the user has
     if (flag === "FromMain") {
         return (
             <>
                 {
-                    sortedData.length !== 0 ? (
+                    loading === false ? (
                         <FlatList
                             data={sortedData}
                             keyboardDismissMode="on-drag"
                             keyboardShouldPersistTaps={'always'}
                             renderItem={({ item, index }) => <MessageMainItem
                                 item={item} index={index}
-                                userData={userData}
+                                userData={userData} t={t}
                                 onUpdateLastMessage={updateLastMessage}
                                 flag={"FromMain"}
                                 theme={theme}
@@ -24,7 +25,11 @@ const MessageMainList = ({ updateLastMessage, userData, sortedData, usersForMess
                             keyExtractor={item => item.owner_uid.toString()}
                         />
                     ) : (
-                        <MessageLoadingPlaceHolder  theme={theme}/>
+                        loading === null ? (
+                            <EmptyDataParma SvgElement={"EditIllustration"} theme={theme} t={t} TitleDataMessage={"Start new chat"} dataMessage={"Send your friend message. You can always come back and check new messages."} />
+                        ) : (
+                            <MessageLoadingPlaceHolder theme={theme} />
+                        )
                     )
                 }
             </>
@@ -34,7 +39,7 @@ const MessageMainList = ({ updateLastMessage, userData, sortedData, usersForMess
         return (
             <>
                 {
-                    usersForMessaging.length !== 0 ? (
+                    loading === false ? (
                         <FlatList
                             data={usersForMessaging}
                             keyboardDismissMode="on-drag"
@@ -43,10 +48,12 @@ const MessageMainList = ({ updateLastMessage, userData, sortedData, usersForMess
                                 item={item} index={index}
                                 userData={userData}
                                 flag={"FromNewMessage"}
-                                theme={theme}
+                                theme={theme} t={t}
                             />}
                             keyExtractor={item => item.owner_uid.toString()}
                         />
+                    ) : loading === null ? (
+                        <EmptyDataParma SvgElement={"AddUserIllustration"} theme={theme} t={t} TitleDataMessage={"Make new connections"} dataMessage={"Connect with new people to start messaging. All new following will appear here."} />
                     ) : (
                         <MessageLoadingPlaceHolder theme={theme} />
                     )
