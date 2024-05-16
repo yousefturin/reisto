f180003db7287e1c8723f1038441838c2ae6858d
yousefturin <yusefturin@gmail.com>
2024-05-16 17:01:44 +0300

    refactor: Update FormatCreateAt.js to format the given date into a string representation of the month and year

--- src/utils/FormatCreateAt.js
+++ src/utils/FormatCreateAt.js
@@ -1,3 +1,9 @@
+/**
+ * Formats the given date into a string representation of the month and year.
+ *
+ * @param {Date} date - The date to be formatted.
+ * @returns {string} The formatted string representation of the month and year.
+ */
 export const formatCreatedAt = (date) => {
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     const year = date.getFullYear();
