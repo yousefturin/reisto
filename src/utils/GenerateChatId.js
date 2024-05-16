2705c7f7fbf63ebc547a1eefed89bb8d9420ef09
yousefturin <yusefturin@gmail.com>
2024-05-16 17:01:53 +0300

    refactor: Update GenerateChatId.js to generate a chat room ID based on two user IDs

--- src/utils/GenerateChatId.js
+++ src/utils/GenerateChatId.js
@@ -1,3 +1,10 @@
+/**
+ * Generates a chat room ID based on two user IDs.
+ *
+ * @param {string} userId1 - The ID of the first user.
+ * @param {string} userId2 - The ID of the second user.
+ * @returns {string} The generated chat room ID.
+ */
 export const GenerateRoomId = (userId1, userId2) => {
     const sortedIds = [userId2, userId1].sort();
     return roomId = sortedIds.join('-');
