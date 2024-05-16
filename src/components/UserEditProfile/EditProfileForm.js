7dd6888acbfe8307506807f264a92554e5783ac2
yousefturin <yusefturin@gmail.com>
2024-05-16 16:55:28 +0300

    refactor: Update EditProfileForm component with internationalization support

--- src/components/UserEditProfile/EditProfileForm.js
+++ src/components/UserEditProfile/EditProfileForm.js
@@ -3,9 +3,8 @@ import React, { useContext, useState } from 'react'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Divider } from 'react-native-elements';
 import { UserContext } from '../../context/UserDataProvider';
-import { colorPalette } from '../../Config/Theme';
 
-const EditProfileForm = ({ navigation, theme }) => {
+const EditProfileForm = ({ navigation, theme, t }) => {
     const [isNamePressed, setIsNamePressed] = useState(false);
     const [isBioPressed, setIsBioPressed] = useState(false);
     const [isLinkPressed, setIsLinkPressed] = useState(false);
@@ -46,23 +45,27 @@ const EditProfileForm = ({ navigation, theme }) => {
     const handleNavigationPress = (id) => {
         let key;
         let value;
+        let headerTitleForScreen;
         switch (id) {
             case 'Name':
                 key = 'Name';
+                headerTitleForScreen = t('screens.profile.text.profileEdit.name');
                 value = userData.displayed_name ? userData.displayed_name : '';
                 break;
             case 'Bio':
                 key = 'Bio';
+                headerTitleForScreen = t('screens.profile.text.profileEdit.bio');
                 value = userData.bio ? userData.bio : '';
                 break;
             case 'Link':
                 key = 'Link';
+                headerTitleForScreen = t('screens.profile.text.profileEdit.link');
                 value = userData.link ? userData.link : '';
                 break;
             default:
                 break;
         }
-        navigation.navigate("UserEditProfileIndividualData", { userData, key, value });
+        navigation.navigate("UserEditProfileIndividualData", { userData, key, value, headerTitleForScreen });
     }
     return (
         <View style={{}}>
@@ -72,15 +75,15 @@ const EditProfileForm = ({ navigation, theme }) => {
                     onPressOut={() => handlePressOut('Name')}
                     onPress={() => handleNavigationPress('Name')}
                     activeOpacity={1}>
-                    <Text style={{ fontSize: 18, fontWeight: "400", color: theme.textSubPrimary, marginVertical: 15, width: "25%", marginLeft: 20 }}>Name</Text>
-                    <Text style={{ fontSize: 18, fontWeight: "400", color: isNamePressed ? theme.btnPrimary : userData.displayed_name ? theme.textSubPrimary : theme.Primary === "#050505" ? theme.Secondary : theme.dividerPrimary, marginVertical: 15, width: "75%" }}>{userData.displayed_name ? userData.displayed_name : "Name"}</Text>
+                    <Text style={{ fontSize: 18, fontWeight: "400", color: theme.textSubPrimary, marginVertical: 15, width: "25%", marginLeft: 20 }}>{t('screens.profile.text.profileEdit.name')}</Text>
+                    <Text style={{ fontSize: 18, fontWeight: "400", color: isNamePressed ? theme.btnPrimary : userData.displayed_name ? theme.textSubPrimary : theme.Primary === "#050505" ? theme.Secondary : theme.dividerPrimary, marginVertical: 15, width: "75%" }}>{userData.displayed_name ? userData.displayed_name : t('screens.profile.text.profileEdit.name')}</Text>
                 </TouchableOpacity>
                 <Divider width={0.5} orientation='horizontal' color={theme.dividerPrimary} style={{ width: "65%", alignSelf: "flex-end", marginHorizontal: 20 }} />
             </View>
             <View style={{ flexDirection: "column", }}>
                 <TouchableOpacity style={{ flexDirection: "row" }}
                     activeOpacity={1}>
-                    <Text style={{ fontSize: 18, fontWeight: "400", color: theme.textSubPrimary, marginVertical: 15, width: "25%", marginLeft: 20 }}>Username</Text>
+                    <Text style={{ fontSize: 18, fontWeight: "400", color: theme.textSubPrimary, marginVertical: 15, width: "25%", marginLeft: 20 }}>{t('screens.profile.text.profileEdit.username')}</Text>
                     <Text style={{ fontSize: 18, color: theme.textPrimary, marginVertical: 15, width: "75%" }}>{userData.username}</Text>
                 </TouchableOpacity>
                 <Divider width={0.5} orientation='horizontal' color={theme.dividerPrimary} style={{ width: "65%", alignSelf: "flex-end", marginHorizontal: 20 }} />
@@ -91,8 +94,8 @@ const EditProfileForm = ({ navigation, theme }) => {
                     onPressOut={() => handlePressOut('Bio')}
                     onPress={() => handleNavigationPress('Bio')}
                     activeOpacity={1}>
-                    <Text style={{ fontSize: 18, fontWeight: "400", color: theme.textSubPrimary, marginVertical: 15, width: "25%", marginLeft: 20 }}>Bio</Text>
-                    <Text style={{ fontSize: 18, fontWeight: "400", color: isBioPressed ? theme.btnPrimary : userData.bio ? theme.textSubPrimary : theme.Primary === "#050505" ? theme.Secondary : theme.dividerPrimary, marginVertical: 15, width: "75%" }}>{userData.bio ? userData.bio : "Bio"}</Text>
+                    <Text style={{ fontSize: 18, fontWeight: "400", color: theme.textSubPrimary, marginVertical: 15, width: "25%", marginLeft: 20 }}>{t('screens.profile.text.profileEdit.bio')}</Text>
+                    <Text style={{ fontSize: 18, fontWeight: "400", color: isBioPressed ? theme.btnPrimary : userData.bio ? theme.textSubPrimary : theme.Primary === "#050505" ? theme.Secondary : theme.dividerPrimary, marginVertical: 15, width: "75%" }}>{userData.bio ? userData.bio : t('screens.profile.text.profileEdit.bio')}</Text>
                 </TouchableOpacity>
                 <Divider width={0.5} orientation='horizontal' color={theme.dividerPrimary} style={{ width: "65%", alignSelf: "flex-end", marginHorizontal: 20 }} />
             </View>
@@ -101,8 +104,8 @@ const EditProfileForm = ({ navigation, theme }) => {
                 onPressOut={() => handlePressOut('Link')}
                 onPress={() => handleNavigationPress('Link')}
                 activeOpacity={1}>
-                <Text style={{ fontSize: 18, fontWeight: "400", color: theme.textSubPrimary, marginVertical: 15, width: "25%", marginLeft: 20 }}>Link</Text>
-                <Text style={{ fontSize: 18, fontWeight: "400", color: isLinkPressed ? theme.btnPrimary : userData.link ? theme.textSubPrimary : theme.Primary === "#050505" ? theme.Secondary : theme.dividerPrimary, marginVertical: 15, width: "75%" }}>{userData.link ? userData.link : "Link"}</Text>
+                <Text style={{ fontSize: 18, fontWeight: "400", color: theme.textSubPrimary, marginVertical: 15, width: "25%", marginLeft: 20 }}>{t('screens.profile.text.profileEdit.link')}</Text>
+                <Text style={{ fontSize: 18, fontWeight: "400", color: isLinkPressed ? theme.btnPrimary : userData.link ? theme.textSubPrimary : theme.Primary === "#050505" ? theme.Secondary : theme.dividerPrimary, marginVertical: 15, width: "75%" }}>{userData.link ? userData.link : t('screens.profile.text.profileEdit.link')}</Text>
             </TouchableOpacity>
         </View>
     )
