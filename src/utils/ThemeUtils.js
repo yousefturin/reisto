e1f89069541e9069a46a3fbdc2f09f7b37d8b54f
yousefturin <yusefturin@gmail.com>
2024-05-16 17:02:12 +0300

    refactor: Update getColorForTheme function to support system theme and improve code readability

--- src/utils/ThemeUtils.js
+++ src/utils/ThemeUtils.js
@@ -1,6 +1,14 @@
 
 import { Appearance } from 'react-native';
 
+/**
+ * Returns the color theme based on the provided parameters.
+ *
+ * @param {Object} value - The object containing the color values for both dark and light themes.
+ * @param {string} selectedTheme - The selected theme ("dark" or "light").
+ * @param {boolean} systemTheme - The flag indicating whether the system theme is enabled.
+ * @returns {string} - The color theme based on the provided parameters.
+ */
 export const getColorForTheme = (value, selectedTheme, systemTheme) => {
     const theme = systemTheme ? Appearance.getColorScheme() === "dark"
         ? value.dark
