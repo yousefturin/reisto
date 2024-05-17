cfcb6bd02e753ed5346abf6f1dfbb29906639d17
yousefturin <yusefturin@gmail.com>
2024-05-17 23:48:28 +0300

    refactor: Update SavedPostsGrid to include support for pull-to-refresh functionality

--- src/components/SavedPosts/SavedPostsGrid.js
+++ src/components/SavedPosts/SavedPostsGrid.js
@@ -1,16 +1,26 @@
 
-import { FlatList, StyleSheet, Dimensions, TouchableOpacity, Animated } from 'react-native';
-import React, { useRef, useState } from 'react'
+import { FlatList, StyleSheet, Dimensions, TouchableOpacity, Animated, View, RefreshControl } from 'react-native';
+import React, { useCallback } from 'react'
 import { Image } from 'expo-image';
 
 import { useNavigation } from "@react-navigation/native";
-import { blurHash } from '../../../assets/HashBlurData';
+
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+
 const screenWidth = Dimensions.get('window').width;
 const columnCount = 3;
-const gapSize = 1;
 
 
-const SavedPostsGrid = ({ posts, userData, navigateToScreen }) => {
+
+const SavedPostsGrid = ({
+    fromWhereValue,
+    posts, userData,
+    navigateToScreen, onRefresh,
+    refreshing,
+    scrollY,
+    onMomentumScrollBegin,
+    onMomentumScrollEnd,
+    onScrollEndDrag }) => {
     const navigation = useNavigation();
     const handleNavigationToPost = (postId) => {
         if (navigateToScreen === "SavedPosts") {
@@ -23,51 +33,102 @@ const SavedPostsGrid = ({ posts, userData, navigateToScreen }) => {
             });
         }
     }
+    const renderItem = useCallback(
+        ({ item }) => (
+            <TouchableOpacity style={styles.listContainer} activeOpacity={0.8} onPress={() => handleNavigationToPost(item.id)}>
+                <View style={styles.imageContainer}>
+                    <Image
+                        source={{ uri: item.imageURL, cache: "force-cache" }}
+                        style={styles.image}
+                        contentFit="cover"
+                        cachePolicy={"memory-disk"}
+                        recyclingKey={item.imageURL}
+                        transition={50}
+                    />
+                </View>
 
-    const renderItem = ({ item }) => (
-        <TouchableOpacity activeOpacity={0.8} onPress={() => handleNavigationToPost(item.id)}>
-            <Image
-                source={{ uri: item.imageURL, cache: "force-cache" }}
-                style={[styles.image]}
-                // placeholder={blurHash}
-                contentFit="cover"
-                cachePolicy={"memory-disk"}
-                recyclingKey={item.imageURL}
-                transition={50}
-            // onLoad={onLoad}
-            />
-        </TouchableOpacity>
-    );
+            </TouchableOpacity>
+        ),
+        []
+    )
     const keyExtractor = (item, index) => {
         if (item.id !== undefined && item.id !== null) {
             return item.id.toString();
         }
         return index.toString();
     };
-    return (
-        <FlatList        
-            keyboardDismissMode="on-drag"
-            keyboardShouldPersistTaps={'always'}
-            data={posts}
-            nestedScrollEnabled={true}
-            scrollEnabled={false}
-            renderItem={renderItem}
-            keyExtractor={keyExtractor}
-            numColumns={columnCount}
-            contentContainerStyle={styles.container}
-        />
-    )
+    //  coming from search
+    if (fromWhereValue !== 0) {
+        return (
+            <AnimatedFlatList
+                style={{ paddingTop: fromWhereValue }}
+                keyboardDismissMode="on-drag"
+                keyboardShouldPersistTaps={'always'}
+                showsVerticalScrollIndicator={false}
+                data={posts}
+                nestedScrollEnabled={true}
+                scrollEnabled={true}
+                renderItem={renderItem}
+                keyExtractor={keyExtractor}
+                numColumns={columnCount}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                    />
+                }
+                onScroll={Animated.event(
+                    [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+                    { useNativeDriver: true }
+                )}
+                onMomentumScrollBegin={onMomentumScrollBegin}
+                onMomentumScrollEnd={onMomentumScrollEnd}
+                onScrollEndDrag={onScrollEndDrag}
+                scrollEventThrottle={4}
+                contentContainerStyle={{ paddingBottom: (screenWidth + 4 - (columnCount)) / 3 }}
+            />
+        )
+        // any place else
+    }else{
+        return(
+            <FlatList
+                style={{ paddingTop: fromWhereValue }}
+                keyboardDismissMode="on-drag"
+                keyboardShouldPersistTaps={'always'}
+                data={posts}
+                nestedScrollEnabled={true}
+                scrollEnabled={true}
+                renderItem={renderItem}
+                keyExtractor={keyExtractor}
+                numColumns={columnCount}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                    />
+                }
+                contentContainerStyle={{ paddingBottom: (screenWidth + 4 - (columnCount)) / 3 }}
+            />
+        )
+    }
+
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'column',
-        paddingVertical: gapSize,
+    listContainer: {
+        flex: 1,
+        margin: 0.3,
+        overflow: 'hidden',
+
+    },
+    imageContainer: {
+        flex: 1,
+        margin: 0.3,
+        overflow: 'hidden',
     },
     image: {
-        width: (screenWidth - (columnCount + 1) * gapSize) / columnCount,
-        height: (screenWidth - (columnCount + 1) * gapSize) / columnCount,
-        margin: gapSize,
+        width: (screenWidth + 4 - (columnCount)) / 3,
+        height: (screenWidth + 4 - (columnCount)) / 3,
     },
 });
 export default SavedPostsGrid
