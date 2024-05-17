aa8729f4074b6a257f616fc8b265e850f167a662
yousefturin <yusefturin@gmail.com>
2024-05-17 23:47:26 +0300

    refactor: Update MessageMainHeader to include opacity prop for animated view

--- src/components/Message/MessageMainHeader.js
+++ src/components/Message/MessageMainHeader.js
@@ -1,10 +1,10 @@
-import { View, Text, Dimensions, TouchableOpacity } from 'react-native'
+import { View, Text, Dimensions, TouchableOpacity, Animated } from 'react-native'
 import React from 'react'
 import SvgComponent from '../../utils/SvgComponents'
 import initializeScalingUtils from '../../utils/NormalizeSize';
 import { useNavigation } from "@react-navigation/native";
 
-const MessageMainHeader = ({ userData, excludedUsers, theme }) => {
+const MessageMainHeader = ({ userData, excludedUsers, theme, opacity }) => {
     const { moderateScale } = initializeScalingUtils(Dimensions);
     const navigation = useNavigation();
     // This function will navigate to the new message screen
@@ -12,19 +12,19 @@ const MessageMainHeader = ({ userData, excludedUsers, theme }) => {
         navigation.navigate("MessagingNewMessageForFollowerAndFollowings", { userData: userData, excludedUsers: excludedUsers, theme: theme });
     }
     return (
-        <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginHorizontal: 10 }}>
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
-                <TouchableOpacity onPress={() => navigation.goBack()}>
-                    <SvgComponent svgKey="ArrowBackSVG" width={moderateScale(40)} height={moderateScale(40)} stroke={theme.textPrimary} />
+            <Animated.View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", opacity: opacity, marginHorizontal: 10 }}>
+                <View style={{ flexDirection: "row", alignItems: "center", }}>
+                    <TouchableOpacity onPress={() => navigation.goBack()}>
+                        <SvgComponent svgKey="ArrowBackSVG" width={moderateScale(40)} height={moderateScale(40)} stroke={theme.textPrimary} />
+                    </TouchableOpacity>
+                    <Text style={{ color: theme.textPrimary, fontWeight: "800", fontSize: 25, }}>{userData.username}</Text>
+                </View>
+                <View style={{ margin: 10, height: moderateScale(30) }}>
+                </View>
+                <TouchableOpacity style={{ marginHorizontal: 10, }} onPress={() => { handleNavigationToNewMessage() }}>
+                    <SvgComponent svgKey="EditSVG" width={moderateScale(24)} height={moderateScale(24)} stroke={theme.textPrimary} />
                 </TouchableOpacity>
-                <Text style={{ color: theme.textPrimary, fontWeight: "800", fontSize: 25, }}>{userData.username}</Text>
-            </View>
-            <View style={{ margin: 10, height: moderateScale(30) }}>
-            </View>
-            <TouchableOpacity style={{ marginHorizontal: 10 }} onPress={() => { handleNavigationToNewMessage() }}>
-                <SvgComponent svgKey="EditSVG" width={moderateScale(24)} height={moderateScale(24)} stroke={theme.textPrimary} />
-            </TouchableOpacity>
-        </View>
+            </Animated.View>
     )
 }
 
