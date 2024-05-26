10a9be989cc4b57a757f9de1c82d0c5080f113e3
yousefturin <yusefturin@gmail.com>
2024-05-26 12:41:53 +0300

    refactor: Update ProfilePost component to handle just seen post highlighting

--- src/components/Profile/ProfilePost.js
+++ src/components/Profile/ProfilePost.js
@@ -1,5 +1,5 @@
 
-import { FlatList, StyleSheet, Dimensions, TouchableOpacity, View } from 'react-native';
+import { FlatList, StyleSheet, Dimensions, TouchableOpacity, View, Text } from 'react-native';
 import React, { useCallback } from 'react'
 import { useNavigation } from "@react-navigation/native";
 import { Image } from 'expo-image';
@@ -9,17 +9,20 @@ const columnCount = 3;
 // OwnerProfilePostScreen will be move inside here, and then based on a state it will be displayed and removed over the screen
 // to keep the data re-rendered since passing the data using routs will make the data as cashed data
 
-const ProfilePost = ({ posts, userData, keyValue, userDataToBeNavigated }) => {
+const ProfilePost = ({ posts, userData, keyValue, userDataToBeNavigated, justSeenPost }) => {
     const navigation = useNavigation();
+    console.log("justSeenPost", justSeenPost)
 
     const handleNavigationToPost = (postId) => {
-        let scrollToIndex 
+        let scrollToIndex
+        justSeenPost = null
         if (posts.length > 0) {
             const index = posts.findIndex(post => post.id === postId);
             if (index !== -1) {
                 scrollToIndex = index;
             }
         }
+
         if (keyValue === "NavigationToMyProfile") {
             navigation.navigate('UserProfilePost', {
                 userData, scrollToPostId: scrollToIndex,
@@ -33,21 +36,36 @@ const ProfilePost = ({ posts, userData, keyValue, userDataToBeNavigated }) => {
 
     const renderItem = useCallback(
         ({ item }) => (
-            <TouchableOpacity disabled={item.empty=== true} style={styles.listContainer} activeOpacity={0.8} onPress={() => handleNavigationToPost(item.id)}>
+            <TouchableOpacity disabled={item.empty === true} style={styles.listContainer} activeOpacity={0.8} onPress={() => handleNavigationToPost(item.id)}>
                 <View style={styles.imageContainer}>
-                    <Image
-                        source={{ uri: item.imageURL, cache: "force-cache" }}
-                        style={styles.image}
-                        contentFit="cover"
-                        cachePolicy={"memory-disk"}
-                        recyclingKey={item.imageURL}
-                        transition={50}
-                    />
+                    {justSeenPost === item.id && justSeenPost !== null && justSeenPost !== undefined ? (
+                        <View style={{ position: "relative", }}>
+                            <Image
+                                source={{ uri: item.imageURL, cache: "force-cache" }}
+                                style={[styles.image, { zIndex: 10, opacity: 0.3 }]}
+                                contentFit="cover"
+                                cachePolicy={"memory-disk"}
+                                recyclingKey={item.imageURL}
+                                transition={50}
+                            />
+                            <Text style={{ position: "absolute", alignSelf: "center", top: "50%", color: "#fff", zIndex: 12, fontWeight: "500", fontSize: 12, margin: -0.3 }}>
+                                Just seen</Text>
+                        </View>
+                    ) : (
+                        <Image
+                            source={{ uri: item.imageURL, cache: "force-cache" }}
+                            style={styles.image}
+                            contentFit="cover"
+                            cachePolicy={"memory-disk"}
+                            recyclingKey={item.imageURL}
+                            transition={50}
+                        />)}
+
                 </View>
 
             </TouchableOpacity>
         ),
-        []
+        [posts]
     )
 
     const keyExtractor = (item, index) => {
@@ -61,15 +79,15 @@ const ProfilePost = ({ posts, userData, keyValue, userDataToBeNavigated }) => {
         // Create a copy of the data array to avoid mutating the original array------------------------------ this shit made me scared a data updated in child
         // will be return to parent and another child will use this data from this child! OMG react really!
         const dataCopy = [...data];
-    
+
         const numberOfFullRows = Math.floor(dataCopy.length / numColumns);
         let numberOfElementsLastRow = dataCopy.length - (numberOfFullRows * numColumns);
-    
+
         while (numberOfElementsLastRow !== numColumns && numberOfElementsLastRow !== 0) {
             dataCopy.push({ key: `blank-${numberOfElementsLastRow}`, empty: true });
             numberOfElementsLastRow++;
         }
-    
+
         return dataCopy;
     };
 
