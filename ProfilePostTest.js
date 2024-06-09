b56a419337dfd4a663b765a5795bb79e40a8a9e2
yousefturin <yusefturin@gmail.com>
2024-06-09 08:17:43 +0300

    update license.

--- ProfilePostTest.js
+++ ProfilePostTest.js
@@ -1,8 +1,20 @@
 /*
- * Copyright (c) 2024 Yusef Rayyan
+ * Copyright (C) 2024 Yusef Rayyan
  *
- * This work is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License.
- * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/4.0/
+ * This file is part of REISTO.
+ *
+ * REISTO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * REISTO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with REISTO. If not, see <https://www.gnu.org/licenses/>.
  */
 import { FlatList, StyleSheet, Dimensions, TouchableOpacity, View, Text } from 'react-native';
 import React, { useCallback } from 'react'
@@ -215,3 +227,5 @@ const styles = StyleSheet.create({
     },
 });
 export default ProfilePost
+
+
